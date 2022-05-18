b968bba5d3a06ae3b4a0c3ac578664b29502d18f
pedro <pedroquintas1213@gmail.com>
2022-05-18 08:35:24 -0300

    SpringBoot

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,31 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.reopository.CursoRepository;
 import br.com.alura.forum.reopository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
+
 public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")
+	@Autowired
+	private CursoRepository cursoRepository;
+	
 	public List<TopicoDto> lista(String nomeCurso){
 		if (nomeCurso==null) {
 			List<Topico> topicos = topicoRepository.findAll();
@@ -27,5 +35,15 @@ public class TopicosController {
 			return TopicoDto.converter(topicos);
 		}
 	}
+
+	@PostMapping
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm form) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
+		
+		return ResponseEntity.created(uri)
+		
+	}		
+	}
 	
-}
+
---
+++ forum/src/main/java/br/com/alura/forum/reopository/CursoRepository.java
@@ -0,0 +1,16 @@
+package br.com.alura.forum.reopository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Curso;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+	static Curso findBynome(String nomeCurso) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
+}
