158cf842e07cf1b153425e276b63d243a2b9fc86
pedro <pedroquintas1213@gmail.com>
2022-05-18 16:09:03 -0300

    SpringBoot

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ public class TopicosController {
 	@Autowired
 	private CursoRepository cursoRepository;
 	
+	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso){
 		if (nomeCurso==null) {
 			List<Topico> topicos = topicoRepository.findAll();
@@ -46,7 +48,8 @@ public class TopicosController {
 		
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
-	}		
 	}
+	
+}
 	
 
